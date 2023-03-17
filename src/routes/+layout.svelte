8f340c8a8e72e5d9e8d967386d466ed3a3beda7c
s1ckret <map-sveltekit@s1ckret.com>
2023-03-17 20:09:11 +0200

    feat: create landing page
    Signed-off-by: s1ckret <map-sveltekit@s1ckret.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<section class="hero is-fullheight">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item is-size-3" href="/">restate-website </a>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="hero-body p-0">
+    <slot />
+  </div>
+
+  <div class="hero-foot">
+    <footer class="footer pb-5 pt-5">
+      <div class="content has-text-centered">
+        <p>Created with &lt;3 by s1ckret</p>
+      </div>
+    </footer>
+  </div>
+</section>
+
+<style lang="scss" global>
+  @import 'static/styles/global.scss';
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container has-text-centered">
+  <h1 class="title is-size-1">Find Your Dream Apartment with rEstate</h1>
+  <h2 class="subtitle">
+    Our product uses advanced object detection technology to make apartment hunting a breeze.
+  </h2>
+  <a href="/map" class="button is-large is-link"> Try Our Map </a>
+</div>
